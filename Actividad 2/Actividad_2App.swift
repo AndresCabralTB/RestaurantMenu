fd195fdc749c60d9c5e1c5092f472d21df3e1ec7
Andres <andres.cabralrs@udlap.mx>
2024-02-01 10:45:12 -0600

    Initial Commit

---
+++ Actividad 2/Actividad_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Actividad_2App.swift
+//  Actividad 2
+//
+//  Created by Andres Cabral on 01/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Actividad_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Actividad 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Actividad 2
+//
+//  Created by Andres Cabral on 01/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
